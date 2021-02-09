ac21a58c0f4039bfe8d8d92164c1fe26588fde3b
CHRISTINA DRACHUK <cdrachuk@gmail.com>
2021-02-09 08:44:25 -0500

    testfile.py

---
+++ testfile.py
@@ -0,0 +1,17 @@
+import string
+import random
+
+word = "hi"
+letters_given = ''
+user_guesses = 7
+
+while user_guesses > 0:
+    letters_left = 0
+    for letter in word:
+        if letter in letters_given:
+            print (letter)
+        else:
+            print("")
+
+alphabet_list = list(string.ascii_lowercase)
+print(alphabet_list)
